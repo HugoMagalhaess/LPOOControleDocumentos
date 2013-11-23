6da35172b5780ed2e7814709fdd8e9a223d7c402
Hugo <Hugo@192.168.25.56>
2013-11-23 10:44:57 -0300

    Commit projeto 2

--- src/Repositorio/classeTeste.java
+++ src/Repositorio/classeTeste.java
@@ -20,7 +20,7 @@ public class classeTeste {
         
         RepositorioList list = new RepositorioList();
         Funcionario func = new Funcionario("Arthur PAdilha","(81) 3333-3333" );
-        Banco banco = new Banco("Bradesco", "Rua Almeida Prado 255","50000-000", "Recife", "PE");
+        Banco banco = new Banco("Bradesco", "Rua Almeida Prado 255","50000-000", "Recife", "PB");
         
         list.init();
        
